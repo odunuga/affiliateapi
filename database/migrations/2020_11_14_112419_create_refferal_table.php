0e336ad86433dc174b6c54b940e6990f7943f33b
odunuga <mcdanell44@gmail.com>
2020-11-14 19:06:19 +0100

    another commit

--- database/migrations/2020_11_14_112419_create_refferal_table.php
+++ database/migrations/2020_11_14_112419_create_refferal_table.php
@@ -17,6 +17,7 @@ class CreateRefferalTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('refcode');
+            $table->timestamps();
         });
     }
 
